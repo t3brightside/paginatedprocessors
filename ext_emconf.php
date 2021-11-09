475119fa4882217c7acab20e8784759350923d58
Tanel Põld <tanel@brightside.ee>
2021-11-09 14:26:44 +0200

    autoload fix

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,23 @@
 <?php
+
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Paginated Processors',
-    'description' => 'Paginated data processors',
-    'category' => 'fe',
-    'author' => 'Tanel Põld',
-    'author_email' => 'tanel@brightside.ee',
-    'author_company' => 'Brightside OÜ / t3brightside.com',
-    'state' => 'stable',
-    'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
+  'title' => 'Paginated Processors',
+  'description' => 'Paginated data processors',
+  'category' => 'fe',
+  'author' => 'Tanel Põld',
+  'author_email' => 'tanel@brightside.ee',
+  'author_company' => 'Brightside OÜ / t3brightside.com',
+  'state' => 'stable',
+  'clearCacheOnLoad' => true,
+  'version' => '1.0.0',
+  'constraints' => [
+    'depends' => [
+      'typo3' => '11.5.0-11.5.99',
     ],
+  ],
+  'autoload' => [
+    'psr-4' => [
+      'Brightside\\Paginatedprocessors\\' => 'Classes'
+    ]
+  ],
 ];
