f29c3ed9892b0e43d1f5f9ea43c2dfd96de10b86
Tanel Põld <tanel@brightside.ee>
2021-11-14 00:23:29 +0200

    [FEATURE] Create TCA for easier 3rd party migration

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,70 @@
+<?php
+  defined('TYPO3_MODE') || die('Access denied.');
+
+  call_user_func(function () {
+/* Define columns added to tt_content */
+    $tempColumnsPaginatedprocessors = array(
+      'tx_paginatedprocessors_paginationenabled' => [
+        'exclude' => 1,
+        'label' => 'Enabled',
+        'config' => [
+            'type' => 'check',
+            'renderType' => 'checkboxToggle',
+            'items' => [
+                [
+                    0 => '',
+                    1 => '',
+                ]
+            ],
+            'behaviour' => [
+              'allowLanguageSynchronization' => true,
+            ],
+        ],
+      ],
+      'tx_paginatedprocessors_itemsperpage' => [
+        'exclude' => 1,
+        'label' => 'Items per page',
+        'config' => [
+          'type' => 'input',
+          'eval' => 'num',
+          'size' => '1',
+          'behaviour' => [
+            'allowLanguageSynchronization' => true,
+          ],
+        ],
+      ],
+      'tx_paginatedprocessors_pagelinksshown' => [
+        'exclude' => 1,
+        'label' => 'Number of links',
+        'config' => [
+          'type' => 'input',
+          'eval' => 'num',
+          'size' => '1',
+          'behaviour' => [
+            'allowLanguageSynchronization' => true,
+          ],
+        ],
+      ],
+      'tx_paginatedprocessors_urlsegment' => [
+        'exclude' => 1,
+        'label' => 'URL Segment',
+        'config' => [
+          'type' => 'input',
+          'eval' => 'uniqueInPid,nospace,lower,trim',
+          'size' => '1',
+          'behaviour' => [
+            'allowLanguageSynchronization' => true,
+          ],
+        ],
+      ]
+    );
+
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumnsPaginatedprocessors);
+    $GLOBALS['TCA']['tt_content']['palettes']['paginatedprocessors']['showitem'] = '
+      tx_paginatedprocessors_paginationenabled,
+      tx_paginatedprocessors_itemsperpage,
+      tx_paginatedprocessors_pagelinksshown,
+      tx_paginatedprocessors_urlsegment,
+    ';
+});
