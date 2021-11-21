89c623ba5264ff6462b5f7f5b4c2382e42372714
Tanel Põld <tanel@brightside.ee>
2021-11-22 00:02:28 +0200

    [TASK] Create Classes/Processing/DataToPaginatedData::paginateData

--- Classes/DataProcessing/PaginatedDatabaseQueryProcessor.php
+++ Classes/DataProcessing/PaginatedDatabaseQueryProcessor.php
@@ -1,16 +1,9 @@
 <?php
-declare(strict_types = 1);
-
 namespace Brightside\Paginatedprocessors\DataProcessing;
 
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor;
-use TYPO3\CMS\Core\Pagination\ArrayPaginator;
-use TYPO3\CMS\Core\Pagination\SimplePagination;
-
-/**
- * Adds pagination interface to the DatabaseQueryProcessor
- */
+use Brightside\Paginatedprocessors\Processing\DataToPaginatedData;
 
 class PaginatedDatabaseQueryProcessor extends DatabaseQueryProcessor
 {
@@ -22,43 +15,9 @@ class PaginatedDatabaseQueryProcessor extends DatabaseQueryProcessor
     ) {
         $allProcessedData = parent::process($cObj, $contentObjectConfiguration, $processorConfiguration, $processedData);
         $paginationSettings = $processorConfiguration['pagination.'];
-        $paginationIsActive = (int)($cObj->stdWrapValue('isActive', $paginationSettings ?? []));
-        if ($paginationIsActive) {
-          $uniquePaginatorId = $cObj->stdWrapValue('uniqueId', $paginationSettings ?? []);
-          $uniquePaginatorIdKey = $cObj->getRequest()->getQueryParams()['paginatorId'];
-          if($uniquePaginatorId == $uniquePaginatorIdKey) {
-            $currentPage = (int)$cObj->getRequest()->getQueryParams()['paginationPage'] ? : 1;
-          } else {
-            $currentPage = 1;
-          }
-          $uniquePaginatorName = $cObj->stdWrapValue('uniquePaginatorName', $paginationSettings ?? []);
-          if ($uniquePaginatorName) {
-            $paginationArray = 'pagination_' . $uniquePaginatorId;
-          } else {
-            $paginationArray = 'pagination';
-          }
-          $itemsToPaginate = $allProcessedData[$processorConfiguration['as']];
-          $itemsPerPage = (int)($cObj->stdWrapValue('itemsPerPage', $paginationSettings ?? [])) ? : 10;
-          $pageLinksShown = (int)($cObj->stdWrapValue('pageLinksShown', $paginationSettings ?? [])) ? : 0;
-          $paginator = new ArrayPaginator($itemsToPaginate, $currentPage, $itemsPerPage);
-          $pagination = new SimplePagination($paginator);
-          $allProcessedData = array_diff_key($allProcessedData, array_flip([$processorConfiguration['as']]));
-          $paginatedData = array(
-            $processorConfiguration['as'] => $paginator->getPaginatedItems(),
-            $paginationArray => array(
-              'uniqueId' => $uniquePaginatorId,
-              'numberOfPages' => $paginator->getNumberOfPages(),
-              'currentPageNumber' => $paginator->getCurrentPageNumber(),
-              'keyOfFirstPaginatedItem' => $paginator->getKeyOfFirstPaginatedItem(),
-              'keyOfLastPaginatedItem' => $paginator->getKeyOfLastPaginatedItem(),
-              'allPageNumbers' => $pagination->getAllPageNumbers(),
-              'previousPageNumber' => $pagination->getPreviousPageNumber(),
-              'nextPageNumber' => $pagination->getNextPageNumber(),
-              'uniquePaginatorName' => $uniquePaginatorName,
-              'pageLinksShown' => $pageLinksShown
-            )
-          );
-          $allProcessedData = array_merge($allProcessedData, $paginatedData);
+        if ((int)($cObj->stdWrapValue('isActive', $paginationSettings ?? []))) {
+          $paginatedData = new DataToPaginatedData();
+          $allProcessedData = $paginatedData->paginateData($cObj,$contentObjectConfiguration,$processorConfiguration,$allProcessedData,$allProcessedData[$processorConfiguration['as']],$processorConfiguration['as']);
           return $allProcessedData;
         } else {
           return $allProcessedData;
---
+++ Classes/Processing/DataToPaginatedData.php
@@ -0,0 +1,56 @@
+<?php
+namespace Brightside\Paginatedprocessors\Processing;
+
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Core\Pagination\ArrayPaginator;
+use TYPO3\CMS\Core\Pagination\SimplePagination;
+
+class DataToPaginatedData {
+  public function paginateData (
+      ContentObjectRenderer $cObj,
+      array $contentObjectConfiguration,
+      array $processorConfiguration,
+      array $allProcessedData,
+      array $dataToPaginate,
+      $paginatedDataArrayKey
+  ) {
+    $paginationSettings = $processorConfiguration['pagination.'];
+
+    $uniquePaginatorId = $cObj->stdWrapValue('uniqueId', $paginationSettings ?? []);
+    $uniquePaginatorIdKey = $cObj->getRequest()->getQueryParams()['paginatorId'];
+    if($uniquePaginatorId == $uniquePaginatorIdKey) {
+      $currentPage = (int)$cObj->getRequest()->getQueryParams()['paginationPage'] ? : 1;
+    } else {
+      $currentPage = 1;
+    }
+    $uniquePaginatorName = $cObj->stdWrapValue('uniquePaginatorName', $paginationSettings ?? []);
+    if ($uniquePaginatorName) {
+      $paginationArray = 'pagination_' . $uniquePaginatorId;
+    } else {
+      $paginationArray = 'pagination';
+    }
+    $itemsToPaginate = $dataToPaginate;
+    $itemsPerPage = (int)($cObj->stdWrapValue('itemsPerPage', $paginationSettings ?? [])) ? : 10;
+    $pageLinksShown = (int)($cObj->stdWrapValue('pageLinksShown', $paginationSettings ?? [])) ? : 0;
+    $paginator = new ArrayPaginator($itemsToPaginate, $currentPage, $itemsPerPage);
+    $pagination = new SimplePagination($paginator);
+    $allProcessedData = array_diff_key($allProcessedData, array_flip([$paginatedDataArrayKey]));
+    $paginatedData = array(
+      $paginatedDataArrayKey => $paginator->getPaginatedItems(),
+      $paginationArray => array(
+        'uniqueId' => $uniquePaginatorId,
+        'numberOfPages' => $paginator->getNumberOfPages(),
+        'currentPageNumber' => $paginator->getCurrentPageNumber(),
+        'keyOfFirstPaginatedItem' => $paginator->getKeyOfFirstPaginatedItem(),
+        'keyOfLastPaginatedItem' => $paginator->getKeyOfLastPaginatedItem(),
+        'allPageNumbers' => $pagination->getAllPageNumbers(),
+        'previousPageNumber' => $pagination->getPreviousPageNumber(),
+        'nextPageNumber' => $pagination->getNextPageNumber(),
+        'uniquePaginatorName' => $uniquePaginatorName,
+        'pageLinksShown' => $pageLinksShown
+      )
+    );
+    $allProcessedData = array_merge($allProcessedData, $paginatedData);
+    return $allProcessedData;
+  }
+}
