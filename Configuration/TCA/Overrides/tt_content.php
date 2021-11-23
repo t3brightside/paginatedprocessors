08a701fcfbeebc347427edf8bd46d25afcc462eb
Tanel Põld <tanel@brightside.ee>
2021-11-23 13:15:25 +0200

    [TASK] Code clean up and comments

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,10 +1,9 @@
 <?php
 
-  defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3_MODE') || die('Access denied.');
 
-  call_user_func(function () {
-      $tempColumnsPaginatedprocessors = array(
-      'tx_paginatedprocessors_paginationenabled' => [
+$tempColumnsPaginatedprocessors = array(
+    'tx_paginatedprocessors_paginationenabled' => [
         'exclude' => 1,
         'label' => 'Enabled',
         'config' => [
@@ -17,54 +16,53 @@
                 ]
             ],
             'behaviour' => [
-              'allowLanguageSynchronization' => true,
+                'allowLanguageSynchronization' => true,
             ],
         ],
-      ],
-      'tx_paginatedprocessors_itemsperpage' => [
+    ],
+    'tx_paginatedprocessors_itemsperpage' => [
         'exclude' => 1,
         'label' => 'Items per page',
         'config' => [
-          'type' => 'input',
-          'eval' => 'num',
-          'size' => '1',
-          'behaviour' => [
-            'allowLanguageSynchronization' => true,
-          ],
+            'type' => 'input',
+            'eval' => 'num',
+            'size' => '1',
+            'behaviour' => [
+                'allowLanguageSynchronization' => true,
+            ],
         ],
-      ],
-      'tx_paginatedprocessors_pagelinksshown' => [
+    ],
+    'tx_paginatedprocessors_pagelinksshown' => [
         'exclude' => 1,
         'label' => 'Number of links',
         'config' => [
-          'type' => 'input',
-          'eval' => 'num',
-          'size' => '1',
-          'behaviour' => [
-            'allowLanguageSynchronization' => true,
-          ],
+            'type' => 'input',
+            'eval' => 'num',
+            'size' => '1',
+            'behaviour' => [
+                'allowLanguageSynchronization' => true,
+            ],
         ],
-      ],
-      'tx_paginatedprocessors_urlsegment' => [
+    ],
+    'tx_paginatedprocessors_urlsegment' => [
         'exclude' => 1,
         'label' => 'URL Segment',
         'config' => [
-          'type' => 'input',
-          'eval' => 'uniqueInPid,nospace,lower,trim',
-          'size' => '1',
-          'behaviour' => [
-            'allowLanguageSynchronization' => true,
-          ],
+            'type' => 'input',
+            'eval' => 'uniqueInPid,nospace,lower,trim',
+            'size' => '1',
+            'behaviour' => [
+                'allowLanguageSynchronization' => true,
+            ],
         ],
-      ]
-    );
+    ]
+);
 
-
-      \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumnsPaginatedprocessors);
-      $GLOBALS['TCA']['tt_content']['palettes']['paginatedprocessors']['showitem'] = '
-      tx_paginatedprocessors_paginationenabled,
-      tx_paginatedprocessors_itemsperpage,
-      tx_paginatedprocessors_pagelinksshown,
-      tx_paginatedprocessors_urlsegment,
-    ';
-  });
+// Use in your custom content element '--palette--;Pagination;paginatedprocessors,'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumnsPaginatedprocessors);
+$GLOBALS['TCA']['tt_content']['palettes']['paginatedprocessors']['showitem'] = '
+    tx_paginatedprocessors_paginationenabled,
+    tx_paginatedprocessors_itemsperpage,
+    tx_paginatedprocessors_pagelinksshown,
+    tx_paginatedprocessors_urlsegment,
+';
