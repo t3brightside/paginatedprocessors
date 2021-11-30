de4c83c818f121220e8ecc36fda1a78846215a31
Tanel Põld <tanel@brightside.ee>
2021-11-30 11:20:34 +0200

    [LANGUAGE] Fix static template title

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -5,5 +5,5 @@ defined('TYPO3_MODE') || die('Access denied.');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'paginatedprocessors',
     'Configuration/TypoScript',
-    'Paginate Processors'
+    'Paginated Processors'
 );
