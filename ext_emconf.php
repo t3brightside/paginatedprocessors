c0c6637e173d2acca72a80ebe5ca25d2032be86b
Tanel Põld <tanel@brightside.ee>
2023-04-26 15:00:43 +0300

    [TASK] Set compatibility to TYPO3 v12.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Brightside OÜ / t3brightside.com',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.5.0',
+    'version' => '1.5.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.9.99',
+            'typo3' => '11.5.0-12.4.99',
             'embedassets' => '1.2.0-1.99.99',
         ],
     ],
